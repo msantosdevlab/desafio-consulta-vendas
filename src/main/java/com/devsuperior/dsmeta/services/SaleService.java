4f2e94d30fb1504e74b5ab054819d93a2987bc5a
msantos-lab <msantosdev.lab@gmail.com>
2024-05-13 10:58:13 +0200

    Service update

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,11 +1,17 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsmeta.dto.ReportDTO;
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -14,10 +20,32 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<ReportDTO> getReport(String minDate, String maxDate, String sellerName, Pageable pageable) {
+		LocalDate minD = verificaDatas(minDate, LocalDate.now().minusYears(1L));
+		LocalDate maxD = verificaDatas(maxDate, LocalDate.now());
+		Page<Sale> result = repository.getReport(minD, maxD, sellerName, pageable);
+		return result.map(x -> new ReportDTO(x));
+	}
+
+	public List<SummaryDTO> getSummary(String minDate, String maxDate, Pageable pageable) {
+		LocalDate minD = verificaDatas(minDate, LocalDate.now().minusYears(1L));
+		LocalDate maxD = verificaDatas(maxDate, LocalDate.now());
+		List<SummaryDTO> result = repository.getSummary(minD, maxD, pageable);
+		return result;
+	}
+
+	private LocalDate verificaDatas(String dataEntrada, LocalDate valorPadrao) {
+		if (dataEntrada != null && !dataEntrada.isEmpty()) {
+			return LocalDate.parse(dataEntrada);
+		} else {
+			return valorPadrao;
+		}
 	}
 }
